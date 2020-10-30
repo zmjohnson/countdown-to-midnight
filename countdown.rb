b89ff9abf343fd6423ccd9158f66eb87b81bd884
Zachary Johnson <zacharymjohnson2@gmail.com>
2020-10-30 19:45:51 -0400

    commit tree c0b8d02f88fec035c0442eeb4e4501f58cf85109

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,15 @@
-#write your code here
+def countdown(count)
+    while count >0
+      puts "#{count} SECOND(S)!"
+      count-=1
+    end
+    "HAPPY NEW YEAR!"
+  end
 
-def countdown
-end
+  def countdown_with_sleep(count)
+    while count > 0
+      count-=1
+      sleep(1)
+    end
+      puts "HAPPY NEW YEAR!"
+  end
